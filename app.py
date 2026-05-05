90e9e0b0946127104b652666c2f8651c2fd799ae
Th1ag0-96 <thiagozobr@protonmail.com>
2026-05-05 18:32:20 -0400

    Meu primeiro site

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Meu Primeiro Site em Python! 🚀")
+st.write("E aí Thi, se você está lendo isso, o Streamlit funcionou no seu Linux Mint!")
+
+if st.button("Clique aqui"):
+    st.balloons()
+    st.success("Funcionou direitinho!")

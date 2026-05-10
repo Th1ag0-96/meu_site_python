6bd07db5298a6f8ae49e909e518b45053ac1f597
Th1ag0-96 <thiagozobr@protonmail.com>
2026-05-09 23:09:07 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title("Meu Primeiro Site em Python! 🚀")
 st.write("E aí Thi, se você está lendo isso, o Streamlit funcionou no seu Linux Mint!")
 st.write("Editando pela primeira vez, bom pelo visto funcionou não?")
+st.write("Vamo ver esse karai")
 if st.button("Clique NESSE KRL"):
     st.balloons()
     st.success("Funcionou direitinho!")

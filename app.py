b1e84e156ac4e0eb51a53ea681cac01377a21701
Th1ag0-96 <thiagozobr@protonmail.com>
2026-05-14 16:04:33 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 import streamlit as st
 
-st.title("Meu Primeiro Site em Python! 🚀")
-st.write("Testando imagem")
-if st.button("Clique aqui pra ver"):
-    st. image("makima.jpeg")
-    st.write("A Makima é uma delicia")
+if "passo" not in st.session_state:
+    st.session_state.passo = 1
+
+st.title("Quiz Interativo🎤✨")
+
+if st.session_state.passo == 1:
+    st.write("## Pergunta 1: O que você mais gosta?")
+    escolha = st.radio("Escolha uma opção:", ["Animes", "Jogos","Programar"], key="pergunta1", index=None)
+    if escolha:
+        st.write(f"✨Que legal que você gosta de {escolha}!")
+
+        if st.button("Proxima"):
+            st.session_state.passo = 2
+            st.rerun()
+
+elif st.session_state.passo == 2:
+    st.write("### Parabens voce conseguiu chrgar na proxima tela")
+    if st.button("Reiniciar"):
+        st.session_state.passo = 1
+        st.rerun()
+        
